2474d7d97545e77a1cf6391a988a2629791112b4
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-01-07 10:42:04 +0530

    - Jan 7, 2020 (1)

--- Tests/LoqqatThemePackTests/LoqqatThemePackTests.swift
+++ Tests/LoqqatThemePackTests/LoqqatThemePackTests.swift
@@ -6,7 +6,7 @@ final class LoqqatThemePackTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(LoqqatThemePack().text, "Hello, World!")
+       // XCTAssertEqual(LoqqatThemePack.text, "Hello, World!")
     }
 
     static var allTests = [
